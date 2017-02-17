359066587464e597d85aaa5fb3121cb72f59bbdb
Miguel Casas-Sanchez <mcasas@chromium.org>
2017-02-16 18:28:04 -0800

    correction in git credentials

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -12,9 +12,9 @@ SHA=`git rev-parse --verify HEAD`
 
 SUPER_REPO="https://$GH_TOKEN@github.com/miguelao/mediacapture-fromelement.git"
 
-git clone $SUPER_REPO
 git config user.name "Travis CI"
 git config user.email "miguelecasassanchez@gmail.com"
+git clone $SUPER_REPO
 
 cd mediacapture-fromelement
 git checkout $TARGET_BRANCH
