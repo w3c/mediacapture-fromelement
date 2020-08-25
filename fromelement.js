37a4d813150d4e02fb162a02c4362a2d1aa171e6
Dominique Hazael-Massieux <dom@w3.org>
2020-08-25 11:17:16 +0200

    Clean up references and markup

--- fromelement.js
+++ fromelement.js
@@ -67,24 +67,14 @@ var respecConfig = {
   //      company: "Your Company", companyURL: "http://example.com/" },
   //],
 
-  // name of the WG
-  wg: ["Web Real-Time Communications Working Group"],
-
-  // URI of the public WG page
-  wgURI: ["https://www.w3.org/2011/04/webrtc/"],
+  group: "webrtc",
 
   // name (without the @w3.org) of the public mailing to which comments are due
   wgPublicList: "public-webrtc",
 
-  // URI of the patent status for this WG, for Rec-track documents
-  // !!!! IMPORTANT !!!!
-  // This is important for Rec-track documents, do not copy a patent URI from a random
-  // document unless you know what you're doing. If in doubt ask your friendly neighbourhood
-  // Team Contact.
-  wgPatentURI: ["https://www.w3.org/2004/01/pp-impl/47318/status"],
-  xref: ['html', 'webidl', 'dom', 'webaudio'],
   testSuiteURI: "https://github.com/web-platform-tests/wpt/tree/master/mediacapture-fromelement",
-
+  github: "https://github.com/w3c/mediacapture-fromelement/",
+  xref: ["html", "webidl", "mediacapture-streams"],
   otherLinks: [
     {
       key: "Participate",
@@ -92,14 +82,6 @@ var respecConfig = {
         {
           value: "Mailing list",
           href: "https://lists.w3.org/Archives/Public/public-webrtc/"
-        },
-        {
-          value: "Browse open issues",
-          href: "https://github.com/w3c/mediacapture-fromelement/issues"
-        },
-        {
-          value: "File a bug",
-          href: "https://github.com/w3c/mediacapture-fromelement/issues/new"
         }
       ]
     }
